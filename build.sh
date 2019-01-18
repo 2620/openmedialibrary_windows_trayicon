724a66fb68bfcd8e6449b02f2b63a1f7dd022b15
j <j@mailb.org>
2019-01-18 19:54:35 +0530

    git via requirements.txt

--- build.sh
+++ build.sh
@@ -3,16 +3,11 @@ BASE=`pwd`
 
 choco install python
 
+python="C:\\Python37\\python.exe"
+
 "C:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\vcvarsall.bat"
 export CL="\"-FIC:\\Program Files (x86)\\Microsoft Visual Studio 14.0\\VC\\include\\stdint.h\" $CL"
-"C:\\Python37\\Scripts\\pip.exe" install pywin32
-python=""C:\\Python37\\python.exe"
-git clone https://github.com/anthony-tuininga/cx_Freeze/
-cd cx_Freeze
-git checkout v6.0b1
-$python setup build
-$python setup install
-cd $BASE
+"C:\\Python37\\Scripts\\pip.exe" install -r requirements.txt
 $python setup.py bdist_msi
 ls -la dist/
 cp dist/*.msi "Open Media Library.msi"
